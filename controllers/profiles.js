9fb212779523e7e0d6cf81c26b59089ec548677e
Tal <trimoni8@gmail.com>
2022-09-30 15:30:58 -0400

    update interest schema

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,1 +1,52 @@
 import { Profile } from "../models/profile.js";
+
+function index(req, res){
+  Profile.find({})
+  .then(profiles => {
+    res.render('profiles/index', {
+      profiles,
+      title: 'Interests'
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect(`/profiles/${req.user.profile._id}`)
+  })
+}
+
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .then(profile => {
+    const isSelf = profile._id.equals(req.user.profile._id)
+    res.render('profiles/show', {
+      title: `Interests${profile.name}'s profile`,
+      isSelf,
+      profile,
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/profiles')
+  })
+}
+
+function createInterest(req, res){
+  Profile.findById(req.user.profile._id)
+  .then(profile => {
+    profile.interests.push(req.body)
+    profile.save()
+    .then(() => {
+      res.redirect(`/profiles/${req.user.profile._id}`)
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect(`/profiles/${req.user.profile._id}`)
+  })
+}
+
+export {
+  index,
+  show,
+  createInterest
+}
--- routes/profiles.js
+++ routes/profiles.js
@@ -4,6 +4,15 @@ import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
+// GET localhost:3000/profiles
+router.get('/', isLoggedIn, profilesCtrl.index)
+
+// GET localhost:3000/:id
+router.get('/:id', isLoggedIn, profilesCtrl.show)
+
+// POST
+router.post('/:id/interests', isLoggedIn, profilesCtrl.createInterest)
+
 export {
   router
 }
