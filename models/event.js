3cff5e8f6ac31678d4d983ff76c1a65205df0bd3
Tal <trimoni8@gmail.com>
2022-10-04 14:32:06 -0400

    work on timing

--- models/event.js
+++ models/event.js
@@ -12,7 +12,13 @@ const commentSchema = new Schema({
 const eventSchema = new Schema({
   name: String,
   details: String,
-  date: Date,
+  date: {
+    type: Date,
+    default: function(){
+      let date = new Date()
+      date.setFullYear(date)
+    }
+  },
   location: String,
   owner: {type: Schema.Types.ObjectId, ref: "Profile"},
   attend: [{type: Schema.Types.ObjectId, ref: "Profile"}],
